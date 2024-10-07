239c6989909221dda2f57efe227fb52941b07ff1
Chris <github@dejong.lu>
2024-10-07 08:37:37 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 # Install ripgrep for telescope live grep search
-sudo apt install ripgrep 
+sudo apt install -y ripgrep 
 # Install python3-venv for ruff formatting
-sudo apt install python3-venv 
+sudo apt install -y python3-venv 
 
 # Download the font and install it 
+sudo apt-get install -y wget
+sudo apt-get install fontconfig
 wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Iosevka.zip
 cd ~/.local/share/fonts
 unzip Iosevka.zip
