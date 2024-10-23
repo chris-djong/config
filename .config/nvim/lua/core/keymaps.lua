e44d8a2b29eaa0da58fa613685da4c4d4edae90f
Chris <chris.dejong@ses.com>
2024-10-23 08:45:32 +0200

    New tabbing behaviour

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,11 +1,11 @@
-vim.g.mapleader=" "
+vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })     -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })   -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })      -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 keymap.set("n", "<leader>sm", "<C-w><C-w>", { desc = "Move to the next screen" }) -- omve to the next screen
 keymap.set("n", "<leader>sh", "<C-w>h", { desc = "Move to the left screen" })
@@ -15,14 +15,14 @@ keymap.set("n", "<leader>sl", "<C-w>l", { desc = "Move to the right screen" })
 
 
 -- tab management
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })                     -- open new tab
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })              -- close current tab
+keymap.set("n", "<leader>th", "<cmd>tabn<CR>", { desc = "Go to next tab" })                     --  go to next tab
+keymap.set("n", "<leader>tl", "<cmd>tabp<CR>", { desc = "Go to previous tab" })                 --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
--- disable arrow keys in normal mode 
-keymap.set("n", "<Up>", "<Nop>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<Down>", "<Nop>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<Left>", "<Nop>", { desc = "Go to previous tab" }) --  go to previous tab
+-- disable arrow keys in normal mode
+keymap.set("n", "<Up>", "<Nop>", { desc = "Close current tab" })                 -- close current tab
+keymap.set("n", "<Down>", "<Nop>", { desc = "Go to next tab" })                  --  go to next tab
+keymap.set("n", "<Left>", "<Nop>", { desc = "Go to previous tab" })              --  go to previous tab
 keymap.set("n", "<Right>", "<Nop>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
