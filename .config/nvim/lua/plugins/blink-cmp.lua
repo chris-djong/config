dfd42c031825e2f775cc095c53746ebf43a5f57e
Chris de Jong <chris@homie.lu>
2025-01-05 22:37:57 +0100

    Added working blink configuration and lazygit

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -1,26 +1,102 @@
 return {
-  'saghen/blink.cmp',
-  -- optional: provides snippets for the snippet source
-  dependencies = 'rafamadriz/friendly-snippets',
+    "saghen/blink.cmp",
+    version = "*",
 
-  version = '*',
-  opts = {
-    -- 'default' for mappings similar to built-in completion
-    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-    -- See the full "keymap" documentation for information on defining your own keymap.
-    keymap = { preset = 'default' },
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+        appearance = {
+            use_nvim_cmp_as_default = false,
+            nerd_font_variant = "mono",
+        },
+        completion = {
+            accept = { auto_brackets = { enabled = true } },
+            documentation = {
+                auto_show = true,
+                auto_show_delay_ms = 250,
+                update_delay_ms = 50,
+                treesitter_highlighting = true,
+                window = { border = "rounded" },
+            },
+            list = {
+                selection = function(ctx)
+                    return ctx.mode == "cmdline" and "auto_insert" or "preselect"
+                end,
+            },
+            menu = {
+                border = "rounded",
+                cmdline_position = function()
+                    if vim.g.ui_cmdline_pos ~= nil then
+                        local pos = vim.g.ui_cmdline_pos -- (1, 0)-indexed
+                        return { pos[1] - 1, pos[2] }
+                    end
+                    local height = (vim.o.cmdheight == 0) and 1 or vim.o.cmdheight
+                    return { vim.o.lines - height, 0 }
+                end,
+                draw = {
+                    columns = {
+                        { "label", "label_description", gap = 1 },
+                        { "kind_icon", "kind" },
+                    },
+                    treesitter = { "lsp" },
+                },
+            },
+        },
 
-    appearance = {
-      use_nvim_cmp_as_default = true,
-      nerd_font_variant = 'mono'
+        -- My super-TAB configuration
+        keymap = {
+            ["<CR>"] = { "accept", "fallback" },
+            ["<Tab>"] = {
+                function(cmp)
+                    return cmp.select_next()
+                end,
+                "snippet_forward",
+                "fallback",
+            },
+            ["<S-Tab>"] = {
+                function(cmp)
+                    return cmp.select_prev()
+                end,
+                "snippet_backward",
+                "fallback",
+            },
+            ["<C-k>"] = { "select_prev", "fallback" },
+            ["<C-j>"] = { "select_next", "fallback" },
+            ["<C-up>"] = { "scroll_documentation_up", "fallback" },
+            ["<C-down>"] = { "scroll_documentation_down", "fallback" },
+        },
+
+
+        sources = {
+            default = { "lsp", "path", "snippets", "buffer" },
+            cmdline = function()
+                local type = vim.fn.getcmdtype()
+                -- Search forward and backward
+                if type == "/" or type == "?" then
+                    return { "buffer" }
+                end
+                -- Commands
+                if type == ":" then
+                    return { "cmdline" }
+                end
+                return {}
+            end,
+            providers = {
+                lsp = {
+                    min_keyword_length = 0, 
+                    score_offset = 0,
+                },
+                path = {
+                    min_keyword_length = 0,
+                },
+                snippets = {
+                    min_keyword_length = 2,
+                },
+                buffer = {
+                    min_keyword_length = 4,
+                    max_items = 5,
+                },
+            },
+        },
     },
-
-    -- Default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
-  },
-  opts_extend = { "sources.default" }
 }
