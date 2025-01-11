da9c3b0c299436e40659693b2d82d38e4d785fcd
Chris <chris@dejong.lu>
2025-01-11 22:47:48 +0100

    Fixed bat theme

--- nixos/home-manager/modules/bat.nix
+++ nixos/home-manager/modules/bat.nix
@@ -4,11 +4,12 @@
     themes = {
       tokyonight_night = {
         src = pkgs.fetchFromGitHub {
-          owner = "folke";
-          repo = "folke";
+	  owner = "folke";
+	  repo = "tokyonight.nvim";
 	  rev = "refs/heads/main";
+	  sha256 = "0aw4qzh4mj592g08nfb9zbviybq7gj3fgkvzw67q0qaficj2lpyl";
         };
-        file = "extras/sublime/tokyonight_night.tmTheme";
+	file = "extras/sublime/tokyonight_night.tmTheme";
       };
     };
   };
