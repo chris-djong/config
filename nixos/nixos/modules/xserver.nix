1b8a6d98f17dd0b6dd738ddcafd63409569a112a
Chris <chris@dejong.lu>
2025-01-11 19:10:22 +0100

    swap ctrl with caps

--- nixos/nixos/modules/xserver.nix
+++ nixos/nixos/modules/xserver.nix
@@ -4,6 +4,7 @@
     desktopManager = {
       gnome.enable = true;
     };
+    xkbOptions = "ctrl:swapcaps";
   };
   services.displayManager.defaultSession = "gnome";
   services.libinput.enable = true;
