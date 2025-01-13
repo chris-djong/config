d9d00bc535ef5e04658b175fe153996a2ea2984c
Chris <chris@dejong.lu>
2025-01-13 19:08:18 +0100

    Added hyprland

--- nixos/hosts/chris-laptop/home-config.nix
+++ nixos/hosts/chris-laptop/home-config.nix
@@ -2,7 +2,19 @@
   nixpkgs.config.allowUnfree = true;
 
   # Import all the modules 
-  imports = [ ../../home-manager/modules ];
+  imports = [
+    ../../home-manager/modules/nvim/nvim.nix
+    ../../home-manager/modules/wezterm/wezterm.nix
+    ../../home-manager/modules/bat.nix
+    ../../home-manager/modules/firefox.nix
+    ../../home-manager/modules/git.nix
+    ../../home-manager/modules/lazygit.nix
+    ../../home-manager/modules/starship.nix
+    ../../home-manager/modules/tmux.nix
+    ../../home-manager/modules/zoxide.nix
+    ../../home-manager/modules/zsh.nix
+    ../../home-manager/modules/hyprland.nix
+  ];
 
   home.packages = with pkgs; [
     # Packages in each category are sorted alphabetically
