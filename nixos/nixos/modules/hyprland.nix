f74d4b03150f2b6b287bdcef95538aaf718d168a
Chris <chris@dejong.lu>
2025-01-13 22:55:21 +0100

    new hypr test

--- nixos/nixos/modules/hyprland.nix
+++ nixos/nixos/modules/hyprland.nix
@@ -1,11 +1,6 @@
 { pkgs, ... }: {
   programs.hyprland.enable = true; # enable Hyprland
 
-  environment.systemPackages = [
-    # ... other packages
-    pkgs.kitty # required for the default Hyprland config
-  ];
+  environment.systemPackages = [ ];
 
-  # Optional, hint Electron apps to use Wayland:
-  # environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
