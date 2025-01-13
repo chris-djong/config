14c1cf78fcf677f8f520356cd4dfde57586ca5e8
Chris <chris@dejong.lu>
2025-01-13 22:58:39 +0100

    fixes for home manager

--- nixos/home-manager/modules/hyprland.nix
+++ nixos/home-manager/modules/hyprland.nix
@@ -5,68 +5,68 @@ with lib; {
   wayland.windowManager.hyprland = {
     enable = true;
 
-    settings = { exec-once = "${startupScript}/bin/start"; };
-
     xwayland.enable = true;
     systemd.enable = true;
 
-    "$mainMod" = "SUPER";
+    settings = {
+      "$mod" = "SUPER";
 
-    bind = [
-      "$mainMod, return, exec, wezterm"
-      "$mainMod, Q, killactive,"
-      "$mainMod SHIFT, M, exit,"
-      "$mainMod SHIFT, F, togglefloating,"
-      "$mainMod, F, fullscreen,"
-      "$mainMod, T, pin,"
-      "$mainMod, G, togglegroup,"
-      "$mainMod, bracketleft, changegroupactive, b"
-      "$mainMod, bracketright, changegroupactive, f"
-      "$mainMod, S, exec, rofi -show drun -show-icons"
-      "$mainMod, P, pin, active"
+      bind = [
+        "$mod, return, exec, wezterm"
+        "$mod, Q, killactive,"
+        "$mod SHIFT, M, exit,"
+        "$mod SHIFT, F, togglefloating,"
+        "$mod, F, fullscreen,"
+        "$mod, T, pin,"
+        "$mod, G, togglegroup,"
+        "$mod, bracketleft, changegroupactive, b"
+        "$mod, bracketright, changegroupactive, f"
+        "$mod, S, exec, rofi -show drun -show-icons"
+        "$mod, P, pin, active"
 
-      ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.4 @DEFAULT_AUDIO_SINK@ 5%+"
-      ",XF86AudioLowerVolume, exec, wpctl set-volume -l 1.4 @DEFAULT_AUDIO_SINK@ 5%-"
+        ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.4 @DEFAULT_AUDIO_SINK@ 5%+"
+        ",XF86AudioLowerVolume, exec, wpctl set-volume -l 1.4 @DEFAULT_AUDIO_SINK@ 5%-"
 
-      "$mainMod, left, movefocus, l"
-      "$mainMod, right, movefocus, r"
-      "$mainMod, up, movefocus, u"
-      "$mainMod, down, movefocus, d"
+        "$mod, left, movefocus, l"
+        "$mod, right, movefocus, r"
+        "$mod, up, movefocus, u"
+        "$mod, down, movefocus, d"
 
-      "$mainMod, h, movefocus, l"
-      "$mainMod, l, movefocus, r"
-      "$mainMod, k, movefocus, u"
-      "$mainMod, j, movefocus, d"
+        "$mod, h, movefocus, l"
+        "$mod, l, movefocus, r"
+        "$mod, k, movefocus, u"
+        "$mod, j, movefocus, d"
 
-      "$mainMod SHIFT, h, movewindow, l"
-      "$mainMod SHIFT, l, movewindow, r"
-      "$mainMod SHIFT, k, movewindow, u"
-      "$mainMod SHIFT, j, movewindow, d"
-    ] ++ map (n:
-      "$mainMod SHIFT, ${toString n}, movetoworkspace, ${
-        toString (if n == 0 then 10 else n)
-      }") [ 1 2 3 4 5 6 7 8 9 0 ] ++ map (n:
-        "$mainMod, ${toString n}, workspace, ${
+        "$mod SHIFT, h, movewindow, l"
+        "$mod SHIFT, l, movewindow, r"
+        "$mod SHIFT, k, movewindow, u"
+        "$mod SHIFT, j, movewindow, d"
+      ] ++ map (n:
+        "$mod SHIFT, ${toString n}, movetoworkspace, ${
           toString (if n == 0 then 10 else n)
-        }") [ 1 2 3 4 5 6 7 8 9 0 ];
+        }") [ 1 2 3 4 5 6 7 8 9 0 ] ++ map (n:
+          "$mod, ${toString n}, workspace, ${
+            toString (if n == 0 then 10 else n)
+          }") [ 1 2 3 4 5 6 7 8 9 0 ];
 
-    binde = [
-      "$mainMod SHIFT, h, moveactive, -20 0"
-      "$mainMod SHIFT, l, moveactive, 20 0"
-      "$mainMod SHIFT, k, moveactive, 0 -20"
-      "$mainMod SHIFT, j, moveactive, 0 20"
+      binde = [
+        "$mod SHIFT, h, moveactive, -20 0"
+        "$mod SHIFT, l, moveactive, 20 0"
+        "$mod SHIFT, k, moveactive, 0 -20"
+        "$mod SHIFT, j, moveactive, 0 20"
 
-      "$mainMod CTRL, l, resizeactive, 30 0"
-      "$mainMod CTRL, h, resizeactive, -30 0"
-      "$mainMod CTRL, k, resizeactive, 0 -10"
-      "$mainMod CTRL, j, resizeactive, 0 10"
-    ];
+        "$mod CTRL, l, resizeactive, 30 0"
+        "$mod CTRL, h, resizeactive, -30 0"
+        "$mod CTRL, k, resizeactive, 0 -10"
+        "$mod CTRL, j, resizeactive, 0 10"
+      ];
 
-    bindm = [
-      # Move/resize windows with mainMod + LMB/RMB and dragging
-      "$mainMod, mouse:272, movewindow"
-      "$mainMod, mouse:273, resizewindow"
-    ];
+      bindm = [
+        # Move/resize windows with mod + LMB/RMB and dragging
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+      ];
+    };
 
   };
 }
