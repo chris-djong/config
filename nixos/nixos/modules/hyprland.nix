e2d8fa5b21d5096184c059b6e5e416ff3cf97f07
Chris <chris@dejong.lu>
2025-01-14 23:31:49 +0100

    added wlogout wallpaper and WIP swaylock

--- nixos/nixos/modules/hyprland.nix
+++ nixos/nixos/modules/hyprland.nix
@@ -1,18 +1,24 @@
 { inputs, config, pkgs, ... }: {
+  programs.hyprland = { enable = true; };
+
   nix.settings = {
     substituters = [ "https://hyprland.cachix.org" ];
     trusted-public-keys =
       [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
   };
 
+  # Needed so that swaylock can go through the PAM check
+  security.pam.services.swaylock = { };
+
   environment.systemPackages = with pkgs; [
-    hyprpaper
-    kitty
-    rofi-wayland
+    wofi # application launcher
     waybar
-    gnome-icon-theme
+    gnome-icon-theme # to show correct mouse icons etc
+    pavucontrol # to control volume
     networkmanagerapplet # to get wifi connectivity
+    wlogout # logout menu with different options
+    swaylock-effects # lock screen
+    nemo # file manager
   ];
 
-  programs.hyprland = { enable = true; };
 }
