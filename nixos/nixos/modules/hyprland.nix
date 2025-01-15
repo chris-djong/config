846e60279b7a4fc5f78933392cd85632ed7661c3
Chris <chris@dejong.lu>
2025-01-15 22:45:24 +0100

    added greetd autologin

--- nixos/nixos/modules/hyprland.nix
+++ nixos/nixos/modules/hyprland.nix
@@ -1,4 +1,4 @@
-{ inputs, config, pkgs, ... }: {
+{ inputs, config, pkgs, services, user, ... }: {
   programs.hyprland = { enable = true; };
 
   nix.settings = {
@@ -10,6 +10,20 @@
   # Create a hyprlock pam file so that we can unlock from here
   security.pam.services.hyprlock = { };
 
+  # Hyprlock is used to authenticate the user. 
+  # Yes I know this is not secure, but my disk is encrypted either way 
+  services.greetd = {
+    enable = true;
+    settings = rec {
+      # Initial session is executed automatically
+      initial_session = {
+        command = "Hyprland";
+        user = user;
+      };
+      default_session = initial_session;
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     wofi # application launcher
     waybar
@@ -20,7 +34,6 @@
     nemo # file manager
     hyprlock # lock screen
     hypridle # automatically lock screen
-    greetd
   ];
 
 }
