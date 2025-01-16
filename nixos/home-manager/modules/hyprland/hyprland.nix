cfedb630237d7608bd32e2c5c4fba739a544bac1
Chris <chris@dejong.lu>
2025-01-16 22:57:51 +0100

    fixed hyprland nix config

--- nixos/home-manager/modules/hyprland/hyprland.nix
+++ nixos/home-manager/modules/hyprland/hyprland.nix
@@ -29,159 +29,157 @@
     };
   };
 
-  wayland.windowManager.hyprland.settings = {
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
 
-    "$mod" = "ALT";
+      "$mod" = "ALT";
 
-    ###################
-    ### MY PROGRAMS ###
-    ###################
+      ###################
+      ### MY PROGRAMS ###
+      ###################
 
-    # See https://wiki.hyprland.org/Configuring/Keywords/
+      # See https://wiki.hyprland.org/Configuring/Keywords/
 
-    # Set programs that you use
-    $terminal = wezterm
-    $fileManager = nemo
-    $menu = wofi --show drun
-    $browser = firefox
+      # Set programs that you use
+      "$terminal" = "wezterm";
+      "$fileManager" = "nemo";
+      "$menu" = "wofi - -show drun";
+      "$browser" = "firefox";
 
-    ################
-    ### MONITORS ###
-    ################
+      ################
+      ### MONITORS ###
+      ################
 
-    monitor = ["eDP-1,highres,0x0,1"];
+      monitor = [ "eDP-1,highres,0x0,1" ];
 
-    #################
-    ### AUTOSTART ###
-    #################
+      #################
+      ### AUTOSTART ###
+      #################
 
-    exec-once = [
-      "waybar"
-      "nm-applet --indicator"
-      "blueman-applet" 
-      "hypridle" 
-      "hyprlock" 
-    ];
+      exec-once = [
+        "waybar"
+        "nm-applet --indicator"
+        "blueman-applet"
+        "hypridle"
+        "hyprlock"
+      ];
 
-    #############################
-    ### ENVIRONMENT VARIABLES ###
-    #############################
+      #############################
+      ### ENVIRONMENT VARIABLES ###
+      #############################
 
-    env = [
-      "XCURSOR_SIZE,12"
-      "HYPRCURSOR_SIZE,12" 
-    ];
+      env = [ "XCURSOR_SIZE,12" "HYPRCURSOR_SIZE,12" ];
 
-    #####################
-    ### LOOK AND FEEL ###
-    #####################
+      #####################
+      ### LOOK AND FEEL ###
+      #####################
 
-    general = {
-      gaps_in = 5;
-      gaps_out = 10;
+      general = {
+        gaps_in = 5;
+        gaps_out = 10;
 
-      border_size = 2;
+        border_size = 2;
 
-      "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg"; 
-      "col.inactive_border" = "rgba(595959aa)"; 
+        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+        "col.inactive_border" = "rgba(595959aa)";
 
-      # Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
-      allow_tearing = false
-      layout = dwindle
-    };
-
-    decoration = {
-      rounding = 4;
-      active_opacity = 1.0;
-      inactive_opacity = 0.9;
-
-      shadow = {
-        enabled = true;
-        range = 4;
-        render_power = 3;
-        color = "rgba(1a1a1aee)" 
+        # Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
+        allow_tearing = false;
+        layout = "dwindle";
       };
 
-      blur = {
-        enabled = true;
-        size = 3;
-        passes = 1;
-        vibrancy = 0.1696
-      };
-    };
+      decoration = {
+        rounding = 4;
+        active_opacity = 1.0;
+        inactive_opacity = 0.9;
 
-    animations = {
+        shadow = {
+          enabled = true;
+          range = 4;
+          render_power = 3;
+          color = "rgba(1a1a1aee)";
+        };
+
+        blur = {
+          enabled = true;
+          size = 3;
+          passes = 1;
+          vibrancy = 0.1696;
+        };
+      };
+
+      animations = {
         enabled = "yes, please :)";
 
         # Default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
-        bezier = "easeOutQuint,0.23,1,0.32,1";
-        bezier = "easeInOutCubic,0.65,0.05,0.36,1";
-        bezier = "linear,0,0,1,1";
-        bezier = "almostLinear,0.5,0.5,0.75,1.0";
-        bezier = "quick,0.15,0,0.1,1";
+        bezier = [
+          "easeOutQuint,0.23,1,0.32,1"
+          "easeInOutCubic,0.65,0.05,0.36,1"
+          "linear,0,0,1,1"
+          "almostLinear,0.5,0.5,0.75,1.0"
+          "quick,0.15,0,0.1,1"
+        ];
 
-        animation = "global, 1, 10, default";
-        animation = "border, 1, 5.39, easeOutQuint";
-        animation = "windows, 1, 4.79, easeOutQuint";
-        animation = "windowsIn, 1, 4.1, easeOutQuint, popin 87%";
-        animation = "windowsOut, 1, 1.49, linear, popin 87%";
-        animation = "fadeIn, 1, 1.73, almostLinear";
-        animation = "fadeOut, 1, 1.46, almostLinear";
-        animation = "fade, 1, 3.03, quick";
-        animation = "layers, 1, 3.81, easeOutQuint";
-        animation = "layersIn, 1, 4, easeOutQuint, fade";
-        animation = "layersOut, 1, 1.5, linear, fade";
-        animation = "fadeLayersIn, 1, 1.79, almostLinear";
-        animation = "fadeLayersOut, 1, 1.39, almostLinear";
-        animation = "workspaces, 1, 2, easeOutQuint, slide";
-        animation = "workspacesIn, 1, 2, easeOutQuint, slide";
-        animation = "workspacesOut, 1, 2, easeOutQuint, slide";
-    };
+        animation = [
+          "global, 1, 10, default"
+          "border, 1, 5.39, easeOutQuint"
+          "windows, 1, 4.79, easeOutQuint"
+          "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
+          "windowsOut, 1, 1.49, linear, popin 87%"
+          "fadeIn, 1, 1.73, almostLinear"
+          "fadeOut, 1, 1.46, almostLinear"
+          "fade, 1, 3.03, quick"
+          "layers, 1, 3.81, easeOutQuint"
+          "layersIn, 1, 4, easeOutQuint, fade"
+          "layersOut, 1, 1.5, linear, fade"
+          "fadeLayersIn, 1, 1.79, almostLinear"
+          "fadeLayersOut, 1, 1.39, almostLinear"
+          "workspaces, 1, 2, easeOutQuint, slide"
+          "workspacesIn, 1, 2, easeOutQuint, slide"
+          "workspacesOut, 1, 2, easeOutQuint, slide"
+        ];
+      };
 
-    dwindle = {
-        pseudotile = true; # Master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+      dwindle = {
+        pseudotile =
+          true; # Master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
         preserve_split = true; # You probably want this
-    };
+      };
 
-    # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
-    master { 
-      new_status = "master";
-    };
+      # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
+      master = { new_status = "master"; };
 
-    # https://wiki.hyprland.org/Configuring/Variables/#misc
-    misc {
-        force_default_wallpaper = -1; # Set to 0 or 1 to disable the anime mascot wallpapers
-        disable_hyprland_logo = false; # If true disables the random hyprland logo / anime girl background. :(
-    };
+      # https://wiki.hyprland.org/Configuring/Variables/#misc
+      misc = {
+        force_default_wallpaper =
+          -1; # Set to 0 or 1 to disable the anime mascot wallpapers
+        disable_hyprland_logo =
+          false; # If true disables the random hyprland logo / anime girl background. :(
+      };
 
-    # https://wiki.hyprland.org/Configuring/Variables/#input
-    input {
+      # https://wiki.hyprland.org/Configuring/Variables/#input
+      input = {
         # TODO: Make internaional keyboard so that "e works 
         kb_layout = "us";
-        kb_variant = null
-        kb_model = null
         kb_options = "caps:swapescape";
-        kb_rules = null;
 
         follow_mouse = 0;
         sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
 
-        touchpad = {
-            natural_scroll = false;
-        };
-    };
+        touchpad = { natural_scroll = false; };
+      };
 
-    gestures = {
-      workspace_swipe= true;
-    };
+      gestures = { workspace_swipe = true; };
 
-    device = {
-      name = "epic-mouse-v1";
-      sensitivity = -0.5;
-    };
+      device = {
+        name = "epic-mouse-v1";
+        sensitivity = -0.5;
+      };
 
-    bind = [ 
-        "$mod, F, exec, firefox" ", Print, exec, grimblast copy area"
+      bind = [
+        "$mod, F, exec, firefox"
+        ", Print, exec, grimblast copy area"
 
         # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
         "$mod, T, exec, $terminal"
@@ -235,8 +233,7 @@
         "$mod SHIFT, 8, movetoworkspace, 8"
         "$mod SHIFT, 9, movetoworkspace, 9"
         "$mod SHIFT, 0, movetoworkspace, 10"
-      ]
-      ++ (
+      ] ++ (
         # workspaces
         # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
         builtins.concatLists (builtins.genList (i:
@@ -247,10 +244,7 @@
           ]) 9));
 
       # Move/resize windows with mainMod + LMB/RMB and dragging
-      bindm = [
-        "$mod, mouse:272, movewindow" 
-        "$mod, mouse:273, resizewindow"
-      ];
+      bindm = [ "$mod, mouse:272, movewindow" "$mod, mouse:273, resizewindow" ];
 
       # Laptop multimedia keys for volume and LCD brightness
       bindel = [
@@ -274,10 +268,12 @@
       ### WINDOWS AND WORKSPACES ###
       ##############################
 
-      # Ignore maximize requests from apps. You'll probably like this.
-      windowrulev2 = "suppressevent maximize, class:.*";
-
-      # Fix some dragging issues with XWayland
-      windowrulev2 = "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0";
+      windowrulev2 = [
+        # Ignore maximize requests from apps. You'll probably like this.
+        "suppressevent maximize, class:.*"
+        # Fix some dragging issues with XWayland
+        "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+      ];
+    };
   };
 }
