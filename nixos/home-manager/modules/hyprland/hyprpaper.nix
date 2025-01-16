d0db86822daa4e0250e0f7658779a6c2ed526473
Chris <chris@dejong.lu>
2025-01-16 23:51:21 +0100

    added hyprpaper

--- nixos/home-manager/modules/hyprland/hyprpaper.nix
+++ nixos/home-manager/modules/hyprland/hyprpaper.nix
@@ -1,14 +1,12 @@
- services.hyprpaper = {
+{
+  services.hyprpaper = {
     enable = true;
     settings = {
       ipc = "off";
       splash = false;
-      preload = [
-        "$HOME/Pictures/wallpapers/2.jpg"
-      ];
+      preload = [ "$HOME/Pictures/wallpapers/2.jpg" ];
 
-      wallpapers = [
-        ",$HOME/Pictures/wallpapers/2.jpg"
-      ];
+      wallpapers = [ ",$HOME/Pictures/wallpapers/2.jpg" ];
     };
   };
+}
