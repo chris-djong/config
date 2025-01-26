a6b0d2959aaf187744201b912e34e216e80c4214
Chris <chris@dejong.lu>
2025-01-26 22:58:12 +0100

    Finalised prompt

--- nixos/home-manager/modules/shells/prompt.sh
+++ nixos/home-manager/modules/shells/prompt.sh
@@ -1,62 +1,59 @@
 update_ps1() {
   # Get the current directory
-  DIR=""
+  LOGOS=""
+  add_logo() {
+    LOGOS="$LOGOS$1"
+  }
   # Show a lock symbol if directory is read-only
   if [ ! -w "$(pwd)" ]; then
-    DIR="🔒"
+    add_logo "🔒"
+  fi
+  if [[ "$BASH_VERSION" ]]; then
+    add_logo "🐚"
+  elif [[ "$ZSH_VERSION" ]]; then
+    add_logo "🌀"
   fi
 
-  # Check for Python version
-  PYTHON_EXISTS=$(python --version 2>/dev/null) 
-  PYTHON_PATH=$(which python 2>/dev/null)
-  PYTHON=""
-  if [[ "$PYTHON_EXISTS" && "$PYTHON_PATH" == *".venv"* ]]; then
-    PYTHON="🐍"
-  fi
-
-  # Check for direnv environment (Optional)
-  DIR_ENV=""
   # If direnv exists
   if type direnv >/dev/null 2>&1; then
     # And is loaded
     DIRENV_LOADED_STATUS=$(direnv status --json | jq '.state.loadedRC.allowed' ) 
     if [[ "$DIRENV_LOADED_STATUS" == "0" ]]; then
-      DIR_ENV="📂"
+      add_logo "📂"
     fi
   fi
 
-  # Get the current git branch
-  GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
-  GIT=""
-  if [ "$GIT_BRANCH" ]; then
-    GIT="$GIT_BRANCH "
+  # Check for Python version
+  PYTHON_EXISTS=$(python --version 2>/dev/null) 
+  PYTHON_PATH=$(which python 2>/dev/null)
+  if [[ "$PYTHON_EXISTS" && "$PYTHON_PATH" == *".venv"* ]]; then
+    add_logo "🐍"
   fi
 
-  # Get the hostname
-  if [[ $PYTHON || $DIR_ENV ]]; then
-    # Extra space for visibility
-    HOST=" $(hostname) "
-  else
-    HOST="$(hostname) "
+  LOGOS="$LOGOS "
+  HOST_PROMPT=""   
+  if [[ $SSH_CONNECTION ]]; then
+    HOST_PROMPT="$(whoami)@$(hostname) "
   fi
-
-  DIRECTORY="$(echo "$PWD" | rev | cut -d'/' -f1-3 | rev)"
-  CHAR=": "
+  GIT=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+  if [[ "$GIT" ]]; then
+    GIT_PROMPT="$GIT "
+  fi
+  DIRECTORY="$(echo "${PWD/#$HOME/~}" | rev | cut -d'/' -f1-3 | rev): "
 
   if [[ "$BASH_VERSION" ]]; then
-     # Define color codes for Bash
-  COLOR_HOST="\e[38;5;205m"     # Pink (205)
-  COLOR_GIT="\e[38;5;49m"       # Green (49)
-  COLOR_DIRECTORY="\e[38;5;14m" # Blue (14)
-  COLOR_RESET="\e[0m"           # Reset
-
-  PS1="$DIR_ENV$PYTHON${COLOR_HOST}$HOST${COLOR_RESET}${COLOR_GIT}$GIT${COLOR_RESET}${COLOR_DIRECTORY}$DIRECTORY${COLOR_RESET}$CHAR"
-    # PS1="$DIR_ENV$PYTHON$HOST$GIT$DIRECTORY$CHAR"
+    # Define color codes for Bash
+    COLOR_HOST="\e[38;5;205m"     # Pink (205)
+    COLOR_GIT="\e[38;5;49m"       # Green (49)
+    COLOR_DIRECTORY="\e[38;5;14m" # Blue (14)
+    COLOR_RESET="\e[0m"           # Reset
   elif [[ "$ZSH_VERSION" ]]; then
-    PS1="$DIR_ENV$PYTHON%F{205}$HOST%f%F{49}$GIT%f%F{14}$DIRECTORY%f%B$CHAR%b"
-  else
-    PS1="$DIR_ENV$PYTHON$HOST$GIT$DIRECTORY$CHAR"
+    COLOR_HOST="%F{205}"     # Pink (205)
+    COLOR_GIT="%F{49}"       # Green (49)
+    COLOR_DIRECTORY="%F{14}"  # Blue (14)
+    COLOR_RESET="%f"         # Reset
   fi
 
+  PS1="$LOGOS${COLOR_HOST}$HOST_PROMPT${COLOR_RESET}${COLOR_GIT}${GIT_PROMPT}${COLOR_RESET}${COLOR_DIRECTORY}$DIRECTORY${COLOR_RESET}"
 }
 
