62ec15b4c72b2007efab64b6ea9c7e0cf7efdcfd
Chris <chris@dejong.lu>
2025-02-03 22:44:58 +0100

    working nvim cmp

--- nixos/home-manager/modules/nvim/plugins/nvim-cmp.lua
+++ nixos/home-manager/modules/nvim/plugins/nvim-cmp.lua
@@ -1,16 +1,17 @@
 local cmp = require("cmp")
 cmp.setup({
 	completion = {
-		completeopt = "menu,menuone,preview,noselect",
+		completeopt = "menu,menuone,noinsert",
 	},
 	mapping = cmp.mapping.preset.insert({
 		["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
 		["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+		["<Tab>"] = cmp.mapping.select_next_item(), -- next suggestion
 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-		["<C-e>"] = cmp.mapping.abort(), -- close completion window
-		["<CR>"] = cmp.mapping.confirm({ select = false }),
+		["<Esc>"] = cmp.mapping.abort(), -- close completion window
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
 	}),
 	-- sources for autocompletion
 	sources = cmp.config.sources({
