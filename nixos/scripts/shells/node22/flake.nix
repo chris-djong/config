732ff463a141b46a8d05817c96a645fe7e30f028
Chris <git@dejong.lu>
2025-02-03 09:11:58 +0100

    fixed flake

--- nixos/scripts/shells/node22/flake.nix
+++ nixos/scripts/shells/node22/flake.nix
@@ -6,11 +6,6 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      # Make the library path depending on what packages you need
-      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
-        zlib # required by numpy
-        stdenv.cc.cc # required by numpy
-      ]);
     in {
       devShells.${system}.default = with pkgs;
         mkShell { buildInputs = [ pkgs.nodejs_22 ]; };
