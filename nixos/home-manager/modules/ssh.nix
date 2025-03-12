23a512d777c3b8dd110021d99df47a47c4ef5248
Chris <git@dejong.lu>
2025-03-12 13:46:54 +0100

    fixed proxy

--- nixos/home-manager/modules/ssh.nix
+++ nixos/home-manager/modules/ssh.nix
@@ -1,5 +1,11 @@
-{ proxy, lib, ... }: {
+{ proxy, lib, ... }:
+let
+  cleanProxy = if proxy != null then
+    builtins.replaceStrings [ "http://" ] [ "" ] proxy
+  else
+    null;
+in {
   programs.ssh.enable = true;
   programs.ssh.matchBlocks."*".proxyCommand =
-    lib.mkIf (proxy != null) "nc -X connect -x ${proxy} %h %p";
+    lib.mkIf (cleanProxy != null) "nc -X connect -x ${cleanProxy} %h %p";
 }
