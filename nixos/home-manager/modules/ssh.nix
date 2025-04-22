af28191f3c8d3a03a33c5439a459ecd8e9f0664d
Chris <git@dejong.lu>
2025-04-22 11:52:13 +0200

    Working SSH FHS fix

--- nixos/home-manager/modules/ssh.nix
+++ nixos/home-manager/modules/ssh.nix
@@ -8,4 +8,11 @@ in {
   programs.ssh.enable = true;
   programs.ssh.matchBlocks."*".proxyCommand =
     lib.mkIf (cleanProxy != null) "nc -X connect -x ${cleanProxy} %h %p";
+  # In general FHS environments the SSH config has wrong permissions because of the nixos store 
+  # To overcome this, we move it to a config_source file on changes and simply rewrite it with a change in permissions
+  home.file.".ssh/config" = {
+    target = ".ssh/config_source";
+    onChange =
+      "cat ~/.ssh/config_source > ~/.ssh/config && chmod 400 ~/.ssh/config";
+  };
 }
