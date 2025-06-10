813b0c4875d8bee73227f42b652c6090b5cbe511
Chris de Jong <chris@dejong.lu>
2025-06-10 23:52:26 +0200

    WIP tmux config

--- nixos/home-manager/modules/tmux.nix
+++ nixos/home-manager/modules/tmux.nix
@@ -38,25 +38,24 @@ in {
 
       # Bind B to copy mode
       bind b copy-mode
-      # V for general copy selection
+
+      # Copy mode key bindings (vi mode)
       bind-key -T copy-mode-vi v send -X begin-selection
-      # Ctrl V for column selection
       bind-key -T copy-mode-vi 'C-v' send -X begin-selection\; send -X rectangle-toggle
-      # Y to copy to clipboard
       bind-key -T copy-mode-vi y send -X copy-pipe-and-cancel "xclip -r -sel clip" 
 
       # Styling for the overal status
-      set -g status-style "bg=${theme.bg} fg=${theme.fg}"
+      set -g status-style "bg=#2A2B3D fg=#c3ccdc"
       set -g status-left "#{?client_prefix,#[fg=${theme.red} bold] PREFIX ,#{?#{==:#{pane_mode},copy-mode},#[fg=${theme.yellow} bold] COPY ,#[fg=${theme.green} bold] NORMAL }}"
       set -g status-right ""
 
       # Styling of the individual panes 
-      set -g pane-border-style 'fg=${theme.fg}'
+      set -g pane-border-style 'fg=#c3ccdc'
       set -g pane-active-border-style 'fg=${theme.green}'
 
       # Styling for the inidividual windows. Note that status means the small tab on the bottom of the window
       # The window style is the full window 
-      set -g window-status-style 'bg=${theme.inactive_bg},fg=${theme.fg}'
+      set -g window-status-style 'bg=${theme.inactive_bg},fg=#c3ccdc'
       set -g window-status-current-style 'bg=${theme.blue},fg=#000000'
       set -g window-status-format " #I: #W "
       set -g window-status-current-format " #I: #W "
