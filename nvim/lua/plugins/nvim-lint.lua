c19950821837cb5976d5fa84b917b0d6d99d11b4
Chris de Jong <chris.de.jong@ses.com>
2025-06-11 16:09:26 +0200

    better linting

--- nvim/lua/plugins/nvim-lint.lua
+++ nvim/lua/plugins/nvim-lint.lua
@@ -15,7 +15,12 @@ lint.linters_by_ft = {
 local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
 -- Show the linting text even when not hovering
-vim.diagnostic.config({ virtual_lines = { current_line = true } })
+vim.diagnostic.config({
+  virtual_text = true,
+  virtual_lines = { current_line = true }
+  ,
+  update_in_insert = false -- we can not update in insert mode because otherwise the autocmd for the virtual_text detection is not triggered
+})
 
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
   group = lint_augroup,
@@ -27,3 +32,44 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 vim.keymap.set("n", "<leader>l", function()
   lint.try_lint()
 end, { desc = "Trigger linting for current file" })
+
+
+-- This autocmd command is used so that virtual text is removed from the current line
+-- Whenever there are some diagnostics. This is so that we do not have both virtuallines and virtualtext visible
+vim.api.nvim_create_autocmd({ 'CursorMoved', 'DiagnosticChanged' }, {
+  group = vim.api.nvim_create_augroup('diagnostic_virt_text_hide', {}),
+  callback = function(ev)
+    local lnum, _ = unpack(vim.api.nvim_win_get_cursor(0))
+    lnum = lnum - 1 -- need 0-based index
+
+    local hidden_lnum = vim.b[ev.buf].diagnostic_hidden_lnum
+    if hidden_lnum and hidden_lnum ~= lnum then
+      vim.b[ev.buf].diagnostic_hidden_lnum = nil
+      -- display all the decorations if the current line changed
+      vim.diagnostic.show(nil, ev.buf)
+    end
+
+    for _, namespace in pairs(vim.diagnostic.get_namespaces()) do
+      local ns_id = namespace.user_data.virt_text_ns
+      if ns_id then
+        local extmarks = vim.api.nvim_buf_get_extmarks(ev.buf, ns_id, { lnum, 0 }, { lnum, -1 }, {})
+        for _, extmark in pairs(extmarks) do
+          local id = extmark[1]
+          vim.api.nvim_buf_del_extmark(ev.buf, ns_id, id)
+        end
+
+        if extmarks and not vim.b[ev.buf].diagnostic_hidden_lnum then
+          vim.b[ev.buf].diagnostic_hidden_lnum = lnum
+        end
+      end
+    end
+  end,
+})
+
+-- We redraw the diagnostics whenever the mode has changes
+vim.api.nvim_create_autocmd('ModeChanged', {
+  group = vim.api.nvim_create_augroup('diagnostic_redraw', {}),
+  callback = function()
+    pcall(vim.diagnostic.show)
+  end
+})
