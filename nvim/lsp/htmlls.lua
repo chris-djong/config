4fadda77b5bee3dbefce7acc723aef5564588bb8
Chris de Jong <chris.de.jong@ses.com>
2025-06-17 16:03:30 +0200

    Fixed htmlls

--- nvim/lsp/htmlls.lua
+++ nvim/lsp/htmlls.lua
@@ -1,6 +1,6 @@
 return {
   cmd = { 'vscode-html-language-server', '--stdio' },
-  filetypes = { "html" },
+  filetypes = { "html", 'htmlangular' },
   root_markers = { "package.json", ".git", vim.uv.cwd() },
   single_file_support = true,
   init_options = {
